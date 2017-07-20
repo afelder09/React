06d3d281d000fd38db267d46d5223cb29ad6be28
Felder <afelder@deloitte.com>
2017-07-20 17:43:10 -0400

    Chapter 2

--- Ch02/02_05/finish/src/index.js
+++ Ch02/02_05/finish/src/index.js
@@ -4,7 +4,7 @@ render(
 	<h1 id='title'
 		className='header'
 		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-	HEY WORLD!!	
+	Yo
 	</h1>,
 	document.getElementById('react-container')
 )
